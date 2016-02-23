7e78558534dec5c69fbf3ab5a26c2b5593ce5a55
Maribel <manana@student-PC>
2016-02-23 11:11:41 +0100

    Version 3

--- 16_ejemplo_git/src/principal/Test.java
+++ 16_ejemplo_git/src/principal/Test.java
@@ -5,8 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		int a= 10; 
 		int b = 20;
+		int c = 7;
 		System.out.println(a);
 		System.out.println(a+b);
+		System.out.println(a+b+c);
 
 	}
 
